d9d6fbb81259cfdc63bd52cded56ff07710096da
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-10-16 15:43:48 +0300

    Web server

--- webserver/src/main/kotlin/FileServer.kt
+++ webserver/src/main/kotlin/FileServer.kt
@@ -1,4 +1,5 @@
 import ru.sber.filesystem.VFilesystem
+import ru.sber.filesystem.VPath
 import java.io.IOException
 import java.io.PrintWriter
 import java.net.ServerSocket
@@ -33,73 +34,88 @@ class FileServer {
             // throw UnsupportedOperationException()
 
             // TODO 1) Use socket.accept to get a Socket object
-            val server = ServerSocket(8888)
-            val socket = server.accept()
             socket.use { s ->
-                // читаем от клиента сообщение
-                val reader = s.getInputStream().bufferedReader()
-                val clientRequest = reader.readLine()
+                while (true) {
+                    val socket = s.accept() // блокирующий вызов
+                    socket.use { s ->
+                        /*
+                        * TODO 2) Using Socket.getInputStream(), parse the received HTTP
+                        * packet. In particular, we are interested in confirming this
+                        * message is a GET and parsing out the path to the file we are
+                        * GETing. Recall that for GET HTTP packets, the first line of the
+                        * received packet will look something like:
+                        *
+                        *     GET /path/to/file HTTP/1.1
+                        */
+                        // читаем от клиента сообщение
+                        val reader = s.getInputStream().bufferedReader()
+                        val clientRequest = reader.readLine()
+//                        println(clientRequest)
 
-                // отправляем ответ
-                val writer = PrintWriter(s.getOutputStream())
-                val serverResponse = "Server response: ${clientRequest.uppercase()}"
-                writer.println(serverResponse)
-                writer.flush()
+                        /*
+                        * TODO 3) Using the parsed path to the target file, construct an
+                        * HTTP reply and write it to Socket.getOutputStream(). If the file
+                        * exists, the HTTP reply should be formatted as follows:
+                        *
+                        *   HTTP/1.0 200 OK\r\n
+                        *   Server: FileServer\r\n
+                        *   \r\n
+                        *   FILE CONTENTS HERE\r\n
+                        *
+                        * If the specified file does not exist, you should return a reply
+                        * with an error code 404 Not Found. This reply should be formatted
+                        * as:
+                        *
+                        *   HTTP/1.0 404 Not Found\r\n
+                        *   Server: FileServer\r\n
+                        *   \r\n
+                        *
+                        * Don't forget to close the output stream.
+                        */
+                        // отправляем ответ
+                        val writer = PrintWriter(s.getOutputStream())
+
+                        var serverResponse = ""
+                        // GET /path/to/file HTTP/1.1
+                        if (!clientRequest.contains("GET")) {
+                            serverResponse = notAGetRequest()
+                        } else {
+                            val path = clientRequest.split(" ")[1]
+//        println(path)
+                            val file = fs.readFile(VPath(path))
+                            if (file == null) {
+                                serverResponse = fileNotFound()
+                            } else {
+                                serverResponse = fileContentsHere(file)
+                            }
+                        }
+
+                        println(serverResponse)
+
+                        writer.println(serverResponse)
+                        writer.flush()
+                    }
+                }
             }
-
-
-//            ServerSocket(7777).use {
-//                while (true) {
-//                    val socket = it.accept() // блокирующий вызов
-//
-//
-//                    socket.use { s ->
-//                        // читаем от клиента сообщение
-//                        val reader = s.getInputStream().bufferedReader()
-//                        val clientRequest = reader.readLine()
-//
-//                        // отправляем ответ
-//                        val writer = PrintWriter(s.getOutputStream())
-//                        val serverResponse = "Server response: ${clientRequest.uppercase()}"
-//                        writer.println(serverResponse)
-//                        writer.flush()
-//
-//                    }
-//
-//                }
-//            }
-
-            /*
-            * TODO 2) Using Socket.getInputStream(), parse the received HTTP
-            * packet. In particular, we are interested in confirming this
-            * message is a GET and parsing out the path to the file we are
-            * GETing. Recall that for GET HTTP packets, the first line of the
-            * received packet will look something like:
-            *
-            *     GET /path/to/file HTTP/1.1
-            */
-
-
-            /*
-             * TODO 3) Using the parsed path to the target file, construct an
-             * HTTP reply and write it to Socket.getOutputStream(). If the file
-             * exists, the HTTP reply should be formatted as follows:
-             *
-             *   HTTP/1.0 200 OK\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *   FILE CONTENTS HERE\r\n
-             *
-             * If the specified file does not exist, you should return a reply
-             * with an error code 404 Not Found. This reply should be formatted
-             * as:
-             *
-             *   HTTP/1.0 404 Not Found\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *
-             * Don't forget to close the output stream.
-             */
         }
+    }
+
+    fun notAGetRequest(): String {
+        return "HTTP/1.0 400 Not a GET request \r\n" +
+                "Server: FileServer\r\n" +
+                "\r\n"
+    }
+
+    fun fileContentsHere(file: String): String {
+        return "HTTP/1.0 200 OK\r\n" +
+                "Server: FileServer\r\n" +
+                "\r\n" +
+                "$file\r\n"
+    }
+
+    fun fileNotFound(): String {
+        return "HTTP/1.0 404 Not Found\r\n" +
+                "Server: FileServer\r\n" +
+                "\r\n"
     }
 }
