3cc91c0720be5f5bab20a10acc519849b75b1930
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-10-31 02:53:13 +0300

    Spring MVC

--- spring-mvc/src/test/kotlin/ru/sber/SpringMvcApplicationTests.kt
+++ spring-mvc/src/test/kotlin/ru/sber/SpringMvcApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.example.demo
+package ru.sber
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringMvcApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
